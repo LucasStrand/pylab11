86d889ed69026defb9c0bda1d2e2ce6fcea7d2dc
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2020-12-07 12:15:06 +0100

    woww done

--- lab11.py
+++ lab11.py
@@ -6,10 +6,10 @@ conn = sqlite3.connect('mytest.db')
 
 c = conn.cursor()
 c.execute(''' CREATE TABLE IF NOT EXISTS persons
-                (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name1 TEXT, name2 TEXT)''')
+                (ID INTEGER PRIMARY KEY AUTOINCREMENT, name1 TEXT, name2 TEXT, UNIQUE(name1, name2))''')
 
 c.execute(''' CREATE TABLE IF NOT EXISTS scores
-                (personID INTEGER, task INTEGER, score INTEGER, FOREIGN KEY (personID) REFERENCES persons(ID) ON DELETE CASCADE)''')
+                (personID INTEGER, task TEXT, score TEXT, FOREIGN KEY (personID) REFERENCES persons(ID) ON DELETE CASCADE, UNIQUE(personID, task))''')
 
 with open("score2.txt", "r") as f:
 
@@ -17,26 +17,34 @@ with open("score2.txt", "r") as f:
         line = line.split()
         firstName = line[2]
         lastName = line[3]
-        nameArr = [firstName, lastName]
         score = line[4]
         task = line[1]
-        scoresTasks = [task, score]
 
-        #Adds a first and last name into the database
-        c.execute('INSERT INTO persons (name1, name2) VALUES (?,?)', nameArr)
         #Deletes existing names :)
-        c.execute('DELETE FROM persons WHERE rowid NOT IN (SELECT min(rowid) FROM persons GROUP BY name1, name2)')
+        #c.execute('DELETE FROM persons WHERE rowid NOT IN (SELECT min(rowid) FROM persons GROUP BY name1, name2)')
  
-        #TODO: Add score to scores table
-        c.execute('INSERT INTO scores (task, score) VALUES (?,?)', scoresTasks)
+        c.execute("INSERT OR IGNORE INTO persons (name1, name2) VALUES (?,?)", [firstName, lastName])
+        c.execute("INSERT OR IGNORE INTO scores VALUES ((SELECT ID FROM persons WHERE name1 = ? AND name2 = ?),?, ?)", [firstName, lastName, task, score])
 
-    # for row in c.execute('SELECT * FROM persons ORDER BY ID'):
-    #     print(row)
-    for sRow in c.execute('SELECT * FROM scores ORDER BY personID'):
-        print(sRow)
+    #print out the top 10 players
+    print("...")
+    for i in c.execute("SELECT name1, name2, SUM(score) as s FROM persons JOIN scores ON ID = personID GROUP BY name1, name2 ORDER BY s DESC LIMIT 10"):
+       print(i)
+    print("...")
 
-    # for idk in c.execute('SELECT name1,name2,task,score FROM persons JOIN scores ON id=sID'):
-    #     print(idk)
+    #print out the top 10 most difficult (least yielding) tasks
+    # for i in c.execute("SELECT task, SUM(score) as s FROM scores GROUP BY task ORDER BY s ASC LIMIT 10"):
+    #    print(i)
+    # print("...")
+
+    #print out the tables persons and/or scores
+    #for i in c.execute("SELECT * FROM persons"):
+    #    print(i)
+    #print("...")
+    #for i in c.execute("SELECT * FROM scores"):
+    #    print(i)
+
+
 conn.commit()
 
 conn.close()
